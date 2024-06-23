b73eaf94dab4e290f5b1d428056311800935bb67
billtangts@gmail.com <billtangts@gmail.com>
2024-06-23 15:37:40 +0800

    add seo.js
    seo.js

--- source/conf.py
+++ source/conf.py
@@ -29,8 +29,14 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# 在文档中添加自定义JavaScript文件
+def setup(app):
+    app.add_js_file('seo.js')
+
+
+
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_title = "Your Desired Title"
+html_title = "CenterMold, a professtional Mold Tooling and Plastic Injection Molding Company"
 
